207c5ed22f15da659ec9721d4697286dffec32bb
Navid Kalaei <navidkalaei@gmail.com>
2017-12-13 13:26:31 +0330

    feat: add reducer

---
+++ reducers/index.js
@@ -0,0 +1,19 @@
+import {RECIEVE_ENTRIES, ADD_ENTRY} from '../actions';
+
+const entries = (state={}, action) => {
+    switch (action.type) {
+        case RECIEVE_ENTRIES:
+            return {
+                ...state,
+                ...action.entries
+            }
+        case ADD_ENTRY:
+            return {
+                ...state,
+                ...action.entry
+            }
+        default:
+            return state
+    }
+}
+
