b971a32e2ab01fdb1f764fd104fe8580321d22bf
Navid Kalaei <navidkalaei@gmail.com>
2017-12-14 17:40:20 +0330

    feat: add style to UdaciSlider

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import {View, Text, Slider} from 'react-native'
+import {View, Text, Slider, StyleSheet} from 'react-native'
+import {gray} from '../utils/colors'
 
 export default ({max, unit, step, value, onChange}) => (
-    <View>
+    <View style={styles.row}>
         <Slider
+            style={{flex: 1}}
             value={value}
             step={step}
             maximumValue={max}
             minimumValue={0}
             onValueChange={onChange}
         />
-        <View>
-            <Text>{value}</Text>
-            <Text>{unit}</Text>
+        <View style={styles.metricCounter}>
+            <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+            <Text style={{fontSize: 18, textAlign: 'center', color: gray}}>{unit}</Text>
         </View>
     </View>
 )
+
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
