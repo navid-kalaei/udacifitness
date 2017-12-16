fc5e419ee3e3bfb8acee9404397cd8888b0a663c
Navid Kalaei <navidkalaei@gmail.com>
2017-12-16 19:20:27 +0330

    feat: add style to History

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, Platform, TouchableOpacity} from 'react-native'
 import {connect} from 'react-redux'
 import {receiveEntries, addEntry} from '../actions'
 import {timeToString, getDailyReminderValue} from '../utils/helpers'
 import {fetchCalendarResults} from '../utils/api'
-
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+import {white} from '../utils/colors'
+import DateHeader from './DateHeader'
 
 class History extends Component {
 
@@ -22,15 +24,57 @@ class History extends Component {
             })
     }
 
-    render() {
+    renderItem = ({today, ...metrics}, formattedDate, key) => (
+        <View style={styles.item}>
+            {today
+                ? <Text>{JSON.stringify(today)}</Text>
+                : <Text>{JSON.stringify(metrics)}</Text>}
+
+        </View>
+    )
+
+    renderEmptyDate(formattedDate) {
         return (
             <View>
-                <Text>{JSON.stringify(this.props)}</Text>
+                <Text>No data for this day.</Text>
             </View>
+        )
+    }
+
+    render() {
+
+        const {entries} = this.props
+
+        return (
+            <UdaciFitnessCalendar
+                items={entries}
+                renderItem={this.renderItem}
+                renderEmptyDate={this.renderEmptyDate}
+            />
         )
     }
 }
 
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+
+        }
+    }
+})
 
 const mapStateToProps = (entries) => ({
     entries
